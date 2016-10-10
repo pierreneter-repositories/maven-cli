336691f1c97df3c9cfad487acb14861d62e554d1
Pierre Neter <pierreneter@gmail.com>
2016-10-10 17:21:30 +0700

    edit index.js, delete some misc

--- index.js
+++ index.js
@@ -3,22 +3,9 @@ var exec = require('child_process').exec;
 var target = require('./target');
 const path = require('path');
 
-(function() {
-    var childProcess = require("child_process");
-    var oldSpawn = childProcess.spawn;
-    function mySpawn() {
-        console.log('spawn called');
-        console.log(arguments);
-        var result = oldSpawn.apply(this, arguments);
-        return result;
-    }
-    childProcess.spawn = mySpawn;
-})();
-
 target.init();
 
 var argv = process.argv.splice(2).join(' ');
-// path.join(target.path, name)
 
 module.exports = function(name) {
   exec(path.join(target.path, name + ` ${argv}`), function(err, out) {
